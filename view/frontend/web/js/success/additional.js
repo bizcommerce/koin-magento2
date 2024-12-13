5f871ff24df1ec974f0fa1e29d439b2eedcbb195
Thiago Contardi <thiagocontardi@hotmail.com>
2024-12-13 01:36:49 -0300

    fix: open 3DS in a new tab

--- view/frontend/web/js/success/additional.js
+++ view/frontend/web/js/success/additional.js
@@ -5,23 +5,24 @@ define([
     'use strict';
 
     return function (config) {
-        const modalId = '#koin-modal-success';
-
-        var options = {
-            type: 'popup',
-            modalClass: 'koin-modal-success',
-            responsive: true,
-            innerScroll: true,
-            buttons: [{
-                text: $.mage.__('Close'),
-                class: 'action secondary action-close',
-                click: function () {
-                    this.closeModal();
-                }
-            }]
-        };
-
-        let popup = modal(options, $(modalId));
-        $(modalId).modal('openModal');
+        window.open(config.challengeUrl, '_blank');
+        // const modalId = '#koin-modal-success';
+        //
+        // var options = {
+        //     type: 'popup',
+        //     modalClass: 'koin-modal-success',
+        //     responsive: true,
+        //     innerScroll: true,
+        //     buttons: [{
+        //         text: $.mage.__('Close'),
+        //         class: 'action secondary action-close',
+        //         click: function () {
+        //             this.closeModal();
+        //         }
+        //     }]
+        // };
+        //
+        // let popup = modal(options, $(modalId));
+        // $(modalId).modal('openModal');
     };
 });
