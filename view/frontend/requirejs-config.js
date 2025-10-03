fa1faa96243a9fc24ab7c84022779176842966ae
Antal <gabriel.antal@bizcommerce.com.br>
2025-10-03 17:49:23 -0300

    feat/KOIN-210 creating a bnpl modal after payment fail

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,7 +3,8 @@ var config = {
         '*': {
             'koinFingerprint': 'Koin_Payment/js/fingerprint',
             'koinBnplBanner': 'Koin_Payment/js/koin-bnpl-banner',
-            'koinBnplCart': 'Koin_Payment/js/koin-bnpl-cart'
+            'koinBnplCart': 'Koin_Payment/js/koin-bnpl-cart',
+            'Magento_Checkout/js/model/place-order': 'Koin_Payment/js/model/place-order'
         }
     }
 };
